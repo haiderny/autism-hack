f49d666082fd4b4f0578d1e9e23d7559df6e96c4
varun vajpayee <varunvajpayee@gmail.com>
2019-04-30 14:24:36 -0400

    added user key and api key

--- src/main/java/com/autism/notification/NotificationRepository.java
+++ src/main/java/com/autism/notification/NotificationRepository.java
@@ -8,22 +8,23 @@ import org.slf4j.LoggerFactory;
 public class NotificationRepository implements INotificationRepository {
 
     private static final Logger Logger = LoggerFactory.getLogger(NotificationRepository.class);
+    public static final String MY_APP_API_TOKEN = "aqwmiq3hj8tbqcezp6b689pf7jxfrf";
+    public static final String USER_ID_TOKEN = "uax29rahvszhdtvh2f4phps7evputm";
 
-    
 
     public void toPushover(String message) {
 
         PushoverClient client = new PushoverRestClient();
 
         try {
-            client.pushMessage(PushoverMessage.builderWithApiToken("MY_APP_API_TOKEN")
-                    .setUserId("USER_ID_TOKEN")
+            client.pushMessage(PushoverMessage.builderWithApiToken(MY_APP_API_TOKEN)
+                    .setUserId(USER_ID_TOKEN)
                     .setMessage("testing!")
                     .build());
 
 // push a message with optional fields
-            Status result = client.pushMessage(PushoverMessage.builderWithApiToken("MY_APP_API_TOKEN")
-                    .setUserId("USER_ID_TOKEN")
+            Status result = client.pushMessage(PushoverMessage.builderWithApiToken(MY_APP_API_TOKEN)
+                    .setUserId(USER_ID_TOKEN)
                     .setMessage("testing!")
                     .setDevice("device")
                     .setPriority(MessagePriority.HIGH) // HIGH|NORMAL|QUIET
